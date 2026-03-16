4b71de22a2fa0754882d084dafe93841c92ca302
Sagar D <sagard.cmagalur@gmail.com>
2026-03-16 22:32:09 +0530

    Add langchain qdrant retriver

---
+++ cinerag/retrieval/retriever.py
@@ -0,0 +1,22 @@
+from langchain_qdrant import QdrantVectorStore
+from cinerag.embeddings import embedder
+from cinerag import config
+
+class Retriever:
+
+    def __init__(self):
+        
+        self.embedder = embedder
+        self.vector_store = QdrantVectorStore.from_existing_collection(
+            collection_name=config.VECTOR_COLLECTION_NAME,
+            embedding=self.embedder.embedding_fn,
+            host=config.QDRANT_HOST,
+            port=config.QDRANT_PORT,
+        )
+        pass
+
+    def retrieve_docs(self, query: str) :
+        return self.vector_store.similarity_search(query, k=5)
+
+
+
