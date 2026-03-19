b845f9cf011cb00ed2f5f39cbe2fe92990fbecff
Sagar D <sagard.cmagalur@gmail.com>
2026-03-19 17:06:28 +0530

    bug fix

--- cinerag-ui/next.config.ts
+++ cinerag-ui/next.config.ts
@@ -5,11 +5,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-
-
-sudo yum update -y
-sudo yum install docker -y
-sudo service docker start
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
