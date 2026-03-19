691803c69b7a72da38230f81d8221bd4f9647ab4
Sagar D <sagard.cmagalur@gmail.com>
2026-03-19 18:33:27 +0530

    load env variables on app launch

--- cinerag/api/app.py
+++ cinerag/api/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from cinerag.api.router import router
 from cinerag.logging_config import setup_logging
+import dotenv
 
+dotenv.load_dotenv()
 setup_logging()
+
 
 app = FastAPI(title="CineRAG", description="Movie knowledge RAG chat API")
 app.include_router(router)
